fc9bb7fbb6b99068bf0339b707723e0970bd720c
dnbworks <dooyongnsaakobalderama@gmail.com>
2022-04-13 15:44:30 +0800

    shift to react v17 to support react-beautiful-dnd  and add sample code for useReducer

--- src/models.ts
+++ src/models.ts
@@ -4,3 +4,33 @@ export interface Todo {
   todo: string;
   isDone: boolean;
 }
+
+
+
+// useReducer
+
+
+
+// import { useReducer } from 'react';
+
+// import React from 'react';
+
+// type Actions = 
+//  | { type: 'add', payload: String } 
+//  | { type: 'remove', payload: number }
+//  | { type: 'done', payload: number };
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//   switch(action.type){
+//     case: 'add':
+//       return [];
+//   }
+// }
+// const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
+//   return (
+    
+//   )
+// }
+
+// export default ReducerExample
